620aee52b6473c2c5ed7f44af1b23ba035af260b
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-21 22:20:02 -0300

    Update reset_main.cpp

--- src/reset/sys/reset_sys_advanced/reset_main.cpp
+++ src/reset/sys/reset_sys_advanced/reset_main.cpp
@@ -1,7 +1,11 @@
 #include "reset_ui.h"
 #include "reset_logic.cpp"
+#include "reset_transition.h"
 
 extern "C" void start_reset_sys(){
-    printk("[RESET_SYS] Iniciando reset avançado - primeira parte...\n");
+    printk("[RESET_SYS] Iniciando reset avançado completo...\n");
+
+    ResetUI::init_ui();
+    ResetTransition::transition_boot_to_reset();
     ResetLogic::perform_reset();
 }
