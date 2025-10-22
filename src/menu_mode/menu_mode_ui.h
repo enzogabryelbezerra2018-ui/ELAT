950a1c15c542f987753284b00b39b171fb4d7006
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-21 21:32:14 -0300

    Create menu_mode_ui.h

---
+++ src/menu_mode/menu_mode_ui.h
@@ -0,0 +1,15 @@
+#ifndef ELAT_MENU_MODE_UI_H
+#define ELAT_MENU_MODE_UI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void start_menu_ui(void);
+int framebuffer_detectado(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
