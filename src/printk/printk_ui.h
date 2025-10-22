27dfd980cb076f61c7572782073ddbf10b6e6226
enzogabryelbezerra2018-ui <enzogabryelbezerra2018@gmail.com>
2025-10-21 23:52:28 -0300

    Create printk_ui.h

---
+++ src/printk/printk_ui.h
@@ -0,0 +1,11 @@
+#ifndef PRINTK_UI_H
+#define PRINTK_UI_H
+
+namespace PRINTK_UI {
+
+    void init_ui();
+    void show_message(const char* title, const char* msg);
+
+} // namespace PRINTK_UI
+
+#endif
